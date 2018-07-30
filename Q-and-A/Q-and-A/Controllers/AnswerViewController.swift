298b7f8518160ef3790f7e5dd3985a78b9550511
David Doswell <davidoliverdoswell@icloud.com>
2018-07-30 16:06:41 -0700

    Create AnswerVC skeleton

---
+++ Q-and-A/Q-and-A/Controllers/AnswerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AnswerViewController.swift
+//  Q-and-A
+//
+//  Created by David Oliver Doswell on 7/30/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
