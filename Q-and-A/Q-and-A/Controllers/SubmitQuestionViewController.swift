9704a42c5d1d6e14bd5d94b4f91dfa13006f1ffb
David Doswell <davidoliverdoswell@icloud.com>
2018-07-30 15:53:44 -0700

    Created QuestionCell with outlets and created SubmitQuestionVC

---
+++ Q-and-A/Q-and-A/Controllers/SubmitQuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SubmitQuestionViewController.swift
+//  Q-and-A
+//
+//  Created by David Oliver Doswell on 7/30/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class SubmitQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Q-and-A/Q-and-A/Views/QuestionCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionCell.swift
+//  Q-and-A
+//
+//  Created by David Oliver Doswell on 7/30/18.
+//  Copyright © 2018 David Oliver Doswell. All rights reserved.
+//
+
+import UIKit
+
+class QuestionCell : UITableViewCell {
+    
+    @IBOutlet weak var question: UILabel!
+    
+    @IBOutlet weak var questionOutput: UILabel!
+    
+    @IBOutlet weak var askedBy: UILabel!
+    
+    @IBOutlet weak var askedByOutput: UILabel!
+    
+    @IBOutlet weak var medium: UILabel!
+    
+    
+}
